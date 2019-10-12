d170246981369230e167d8e3ed1f695cd78f1714
VK <vkmsoft@yandex.ru>
2019-10-12 15:43:12 +0300

    DEV: Keycloak use SSL

--- docker/compose-down-erase.sh
+++ docker/compose-down-erase.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker-compose \
-  -f services/network.yml \
+  -f services/network/network-compose.yml \
   -f services/consul/consul-compose.yml \
   -f services/db/db-compose.yml \
   -f services/keycloak/keycloak-compose.yml \
